34b84afe835d8d264a86996e9a05895d6e70eb87
exxxxh <exxxxh@yahoo.com>
2022-04-27 21:27:40 +0200

    second commit

---
+++ main.tf
@@ -0,0 +1,7 @@
+module "vpc" {
+  source           = "./module/vpc/"
+  vpc_cidr_block   = var.vpc_cidr_block
+  instance_tenancy = var.instance_tenancy
+  aws_region = "eu-central-1"
+}
+
---
+++ module/vpc/variable.tf
@@ -0,0 +1,10 @@
+variable "vpc_cidr_block" {
+    type = string
+}
+variable "instance_tenancy" {
+    type = string
+}
+variable "aws_region" {
+    type =string
+}
+   
